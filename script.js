007be47a0418e5fc6e3006c2092adc9e7edcae14
VladimirB <vlbelevich@yandex.by>
2025-01-25 21:08:32 +0300

    Add grid building

--- script.js
+++ script.js
@@ -61,6 +61,31 @@
     ["3", "6", "9"],
   ];
 
+  let gridCells = document.querySelectorAll(".grid div");
+
+  gridCells.forEach((element) => {
+    if (
+      element.getAttribute("data-id") == 1 ||
+      element.getAttribute("data-id") == 2 ||
+      element.getAttribute("data-id") == 3 ||
+      element.getAttribute("data-id") == 4 ||
+      element.getAttribute("data-id") == 5 ||
+      element.getAttribute("data-id") == 6
+    ) {
+      element.style.borderBottom = "1px solid #aaa";
+    }
+    if (
+      element.getAttribute("data-id") == 1 ||
+      element.getAttribute("data-id") == 2 ||
+      element.getAttribute("data-id") == 4 ||
+      element.getAttribute("data-id") == 5 ||
+      element.getAttribute("data-id") == 7 ||
+      element.getAttribute("data-id") == 8
+    ) {
+      element.style.borderRight = "1px solid #aaa";
+    }
+  });
+
   inputData.firstPlayerName.addEventListener("keyup", () => {
     player1.name = inputData.firstPlayerName.value;
     updateResultsTable();
